c1cd5a008189dec852b4d5896071e2b916a9ed68
Poonam FastFurious <your-email@example.com>
2025-02-28 17:11:13 +0530

    update read and deliver

--- src/Modules/Mesaage/Message.routes.js
+++ src/Modules/Mesaage/Message.routes.js
@@ -3,9 +3,6 @@ import { verifyJWT } from "../../middlewares/auth.middlwares.js";
 import {
   allMessages,
   sendMessage,
-  deliveredOn,
-  readOn,
-  pinMessage,
   deleteMessage,
 } from "./Message.controler.js";
 import { upload } from "../../middlewares/FileUpload.middlwares.js";
@@ -22,9 +19,9 @@ router.route("/").post(
 );
 router.route("/:chatId").get(verifyJWT, allMessages);
 router.route("/delete").delete(verifyJWT, deleteMessage);
-router.route("/readon").post(verifyJWT, readOn);
-router.route("/deliverdon").post(verifyJWT, deliveredOn);
-router.route("/pinmessage").patch(pinMessage);
+// router.route("/readon").post(verifyJWT, readOn);
+// router.route("/deliverdon").post(verifyJWT, deliveredOn);
+// router.route("/pinmessage").patch(pinMessage);
 //testData for read on deliverd on
 // {
 //   "messageIds": ["66b76f7c8ae365838590fd8b", "66b76f7c8ae365838590fd85", "66b76f2f8ae365838590fd7e"]
